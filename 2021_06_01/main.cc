76b1855be8c60764a47af89e7de180871a5aa528
MBernart <mateuszbernart@gmail.com>
2021-06-15 09:55:16 +0200

    2021_06_01

---
+++ 2021_06_01/main.cc
@@ -0,0 +1,26 @@
+/**
+ * @file main.cc
+ * @author Mateusz Bernart (mateuszbernart@gmail.com)
+ * @brief reverse polish notation
+ * @version 0.1
+ * @date 2021-06-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> splitStringBySubString()
+{
+
+}
+
+int main(void)
+{
+    cout << "Hello world!\n";
+    return 0;
+}
